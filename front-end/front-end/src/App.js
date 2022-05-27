9cd12cc00aeac56ec75e851a2e5304c03d29d9e9
jholmes847 <93491719+jholmes847@users.noreply.github.com>
2022-05-27 12:49:21 -0700

    commit on frontend

--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -8,7 +8,29 @@ import axios from 'axios'
 
 function App() {
   return (
-    <h1>PlayList</h1>
+    <>
+    <h1 className="title">PlayList</h1>
+    <div className='play-list'> 
+    
+       Artist: <br></br>
+       <input type='text'/>
+       <br></br>
+       Album: <br></br>
+        <input type='text'/>,
+       <br></br>
+       Song: <br></br>
+              <input type='text'/>
+       <br></br>
+       Genre: <br></br>
+       <input type='text'/> 
+       <br></br>
+       Album Cover: <br></br>
+       <input type='url'/>
+       
+    </div>
+    </>
+    
+
   );
 }
 
