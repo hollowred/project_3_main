eae4fbf61d2af6b6c1f18309eb6a9311c3ec6825
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-05-28 20:30:17 -0500

    routes

--- models/songs.js
+++ models/songs.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-const songSchema = new mongoose.Schema ({
-  Artist: String,
-  Album: String,
-  Song: String,
-  Genre: String,  
+const songsSchema = new mongoose.Schema({
+  artist: String,
+  album: String,
+  aong: String,
+  genre: String,
   albumImage: String,
   showEdit: Boolean
 })
 
-const Songs = mongoose.model('Song', songSchema)
+const Song = mongoose.model('Song', songsSchema);
 
-module.exports = Songs
+module.exports = Song;
--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const Song = require('./models/songs.js')
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 
-app.get('/songs', (req, res) => {
-        res.json('Hello')
-    // Monkey.find({}, (err, songsHome) => {
-    //     res.json(songsHome)
+app.post('/songs', (req, res) => {
+    Song.create(req.body, (err, createdSong) => {
+        res.json(createdSong)
     })
-//})
+})
 
+app.get('/songs', (req, res) => {
+     Song.find({}, (err, songsHome) => {
+         res.json(songsHome)
+    })
+})
+
+app.delete('/songs/:id', (req, res)=>{
+    Song.findByIdAndRemove(req.params.id, (err, deletedSong)=>{
+        res.json(deletedSong)
+    })
+})
+
+app.put('/songs/:id', (req,res)=>{
+    Song.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedSong)=>{
+        res.json(updatedSong)
+    })
+})
 
 app.listen(3000, () => {
     console.log('listening...');
