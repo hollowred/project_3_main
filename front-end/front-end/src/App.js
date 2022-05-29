84b3aa84ef76bca6e403554fab0d9ddbbf1fef5d
jholmes847 <93491719+jholmes847@users.noreply.github.com>
2022-05-29 06:50:22 -0700

    commit on front-end

--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 import axios from 'axios'
 
 
@@ -7,29 +8,89 @@ import axios from 'axios'
 
 
 function App() {
+ const [record, setRecord] = useState()
+  const [artist, setArtist] = useState()
+  const [album, setAlbum] = useState()
+
+  const [song, setSong] = useState()
+
+  const [genre, setGenre] = useState()
+
+  const [albumImage, setAlbumImage] = useState()
+
+  
+const handleNewArtist = (event) => {
+  setArtist(event.target.value);
+}
+
+  const handleNewAlbum = (event)=> {
+    setAlbum(event.target.value);
+  }
+
+
+  const handleNewSong = (event)=> {
+    setSong(event.target.value);
+  }
+
+  const handleNewGenre = (event)=> {
+    setGenre(event.target.value);
+  }
+
+  const handleNewAlbumImage = (event)=> {
+    setAlbumImage(event.target.value);
+  }
+
+  const handleNewSongFormSubmit = (event) =>{
+    event.preventDefault()
+  axios.post(
+    'http://localhost:3000/songs',
+    {
+      Artist: artist,
+      Album: album,
+      Song: song,
+      Genre: genre,  
+      albumImage: albumImage,
+     
+    }
+  
+  ).then(() =>{
+    axios
+    .get('http://localhost:3000/cars').then((response) =>{
+      setRecord(response.data)
+    })
+  })
+  }
+
+
+
+
+
+
   return (
     <>
     <h1 className="title">PlayList</h1>
+    
     <div className='play-list'> 
+    <form onSubmit={handleNewSongFormSubmit}>
+    Artist: <input type="text"onChange={handleNewArtist}/><br/>
     
-       Artist: <br></br>
-       <input type='text'/>
+       Album: <input type="text" onChange=
+       {handleNewAlbum}/>
        <br></br>
-       Album: <br></br>
-        <input type='text'/>,
+       Song:  <input type="text" onChange=
+       {handleNewSong}/>
        <br></br>
-       Song: <br></br>
-              <input type='text'/>
+       Genre: <input type="text" onChange=
+       {handleNewGenre}/>
        <br></br>
-       Genre: <br></br>
-       <input type='text'/> 
+       Album Cover:  <input type="text" onChange=
+       {handleNewAlbumImage}/>
        <br></br>
-       Album Cover: <br></br>
-       <input type='url'/>
-       
+       <input type="submit" value="ADD SONG"/>
+    </form>
     </div>
-    </>
-    
+ 
+ </>
 
   );
 }
--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const Songs = require('./models/songs');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 
-app.get('/songs', (req, res) => {
-        res.json('Hello')
-    // Monkey.find({}, (err, songsHome) => {
-    //     res.json(songsHome)
+
+//CREATE
+app.post('/song', (req, res) => {
+    Songs.create(req.body, (err, createdSong) => {
+        res.json(createdSong)
+
     })
-//})
+})
+//INDEX
+app.get('/song', (req, res) => {
+    Songs.find({}, (err, foundSong) => {
+        res.json(foundSong)
+    })
+})
+///DELETE
+app.delete('/song/:id', (req, res)=>{
+    Songs.findByIdAndRemove(req.params.id, (err, deletedSong)=>{
+        res.json(deletedSong)
+    })
+})
+
+////UPDATE
+app.put('/song/:id', (req,res)=>{
+    Songs.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedSong)=>{
+        res.json(updatedSong)
+    })
+
+   })
 
 
 app.listen(3000, () => {
     console.log('listening...');
 })
 
-mongoose.connect('mongodb://localhost:27017/merncrud')
+mongoose.connect('mongodb://localhost:27017/songs')
 mongoose.connection.once('open', () => {
     console.log('connected to mongod...');
 })
