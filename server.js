65d933f72b557381307402a6d2f3270c478591b2
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-05-30 15:55:05 -0500

    edits

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-<<<<<<< HEAD
+
 const Song = require('./models/songs.js')
-=======
-const Song = require('./front-end/front-end/src/models/songs');
->>>>>>> 6844408ec11b2fad41b6073b493fa13b9551721a
+
+
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 
-<<<<<<< HEAD
-app.post('/songs', (req, res) => {
+
+app.post('/song', (req, res) => {
     Song.create(req.body, (err, createdSong) => {
         res.json(createdSong)
     })
 })
 
-app.get('/songs', (req, res) => {
+app.get('/song', (req, res) => {
      Song.find({}, (err, songsHome) => {
          res.json(songsHome)
     })
 })
-=======
+
 
 // //CREATE
 app.post('/song', (req, res) => {
@@ -53,19 +53,7 @@ app.get('/song', (req, res) => {
 //     })
 
 //    })
->>>>>>> 6844408ec11b2fad41b6073b493fa13b9551721a
 
-app.delete('/songs/:id', (req, res)=>{
-    Song.findByIdAndRemove(req.params.id, (err, deletedSong)=>{
-        res.json(deletedSong)
-    })
-})
-
-app.put('/songs/:id', (req,res)=>{
-    Song.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedSong)=>{
-        res.json(updatedSong)
-    })
-})
 
 app.listen(3000, () => {
     console.log('listening...');
