d0af9135c937e35d41273d71abf33c3d6311ddf3
jholmes847 <93491719+jholmes847@users.noreply.github.com>
2022-05-30 10:19:06 -0700

    commit frontend

--- front-end/front-end/src/models/songs.js
+++ front-end/front-end/src/models/songs.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-<<<<<<< HEAD:models/songs.js
 const songsSchema = new mongoose.Schema({
   artist: String,
   album: String,
@@ -11,17 +10,5 @@ const songsSchema = new mongoose.Schema({
 })
 
 const Song = mongoose.model('Song', songsSchema);
-=======
-const songSchema = new mongoose.Schema ({
-  Artist: String,
-  Album: String,
-  Song: String,
-  Genre: String,  
-  albumImage: String,
-  
-})
-
-const Songs = mongoose.model('songs', songSchema)
->>>>>>> 84da5b9 (commit on front end):front-end/front-end/src/models/songs.js
 
 module.exports = Song;
