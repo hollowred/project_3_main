95a62fca67de89d054cb093c0e41e41ebda39d4d
jholmes847 <93491719+jholmes847@users.noreply.github.com>
2022-06-01 09:46:29 -0700

    Update songs.js

--- models/songs.js
+++ models/songs.js
@@ -6,9 +6,11 @@ const songsSchema = new mongoose.Schema({
   song: String,
   genre: String,
   albumImage: String,
+  video: String,
   showEdit: Boolean
 })
 
 const Song = mongoose.model('Song', songsSchema);
 
 module.exports = Song;
+
